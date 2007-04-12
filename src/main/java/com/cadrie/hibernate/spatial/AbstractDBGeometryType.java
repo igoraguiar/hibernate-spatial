2fed35c2b76f3cb73eb29f68bf352224c49d85f2
Karel Maesen <karel@geovise.com>
2007-04-12 16:46:50 +0000

    Corrected file headers (LGPL license)

--- src/main/java/com/cadrie/hibernate/spatial/AbstractDBGeometryType.java
+++ src/main/java/com/cadrie/hibernate/spatial/AbstractDBGeometryType.java
@@ -1,27 +1,31 @@
 /**
  * $Id$
  *
- * This file is part of MAJAS (Mapping with Asynchronous JavaScript and ASVG). a
- * framework for Rich Internet GIS Applications.
+ * This file is part of Spatial Hibernate, an extension to the 
+ * hibernate ORM solution for geographic data. 
+ *  
+ * Copyright © 2007 K.U. Leuven LRD, Spatial Applications Division, Belgium
  *
- * Copyright  @ 2007 DFC Software Engineering, Belgium
- * and K.U. Leuven LRD, Spatial Applications Division, Belgium
+ * This work was partially supported by the European Commission, 
+ * under the 6th Framework Programme, contract IST-2-004688-STP.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * MAJAS is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * MAJAS is distributed in the hope that it will be useful,
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with gGIS; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * For more information, visit: http://www.cadrie.com/
  */
-
+ 
 package com.cadrie.hibernate.spatial;
 
 import java.io.Serializable;
@@ -38,11 +42,10 @@ import org.hibernate.usertype.UserType;
 import com.vividsolutions.jts.geom.Geometry;
 
 /**
- * @author Karel Maesen
- * 
  * This type is a abstract base type for implemenating database specific user
  * types for geometry types.
  * 
+ * @author Karel Maesen
  */
 public abstract class AbstractDBGeometryType implements UserType,
         ParameterizedType {
--- src/main/java/com/cadrie/hibernate/spatial/spi/SpatialDialectProvider.java
+++ src/main/java/com/cadrie/hibernate/spatial/spi/SpatialDialectProvider.java
@@ -1,27 +1,31 @@
 /**
  * $Id$
  *
- * This file is part of MAJAS (Mapping with Asynchronous JavaScript and ASVG). a
- * framework for Rich Internet GIS Applications.
+ * This file is part of Spatial Hibernate, an extension to the 
+ * hibernate ORM solution for geographic data. 
+ *  
+ * Copyright © 2007 K.U. Leuven LRD, Spatial Applications Division, Belgium
  *
- * Copyright  © 2007 DFC Software Engineering, Belgium
- * and K.U. Leuven LRD, Spatial Applications Division, Belgium
+ * This work was partially supported by the European Commission, 
+ * under the 6th Framework Programme, contract IST-2-004688-STP.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * MAJAS is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * MAJAS is distributed in the hope that it will be useful,
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with gGIS; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * For more information, visit: http://www.cadrie.com/
  */
-
+ 
 package com.cadrie.hibernate.spatial.spi;
 
 import java.util.Map;
@@ -37,6 +41,7 @@ import com.cadrie.hibernate.spatial.SpatialDialect;
  * <code>org.hibernate.dialect.Dialect</code> or <code>SpatialDialect</code>
  * implementation
  * 
+ * @author Karel Maesen
  */
 
 public interface SpatialDialectProvider {
