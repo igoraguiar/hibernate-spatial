05249697e41250594a22c49bc68a4bf4871dda4a
Karel Maesen <karel@geovise.com>
2010-03-18 16:08:58 +0000

    HIBSPA-42

--- src/main/java/org/hibernatespatial/mgeom/MultiMLineString.java
+++ src/main/java/org/hibernatespatial/mgeom/MultiMLineString.java
@@ -3,7 +3,7 @@
  *
  * This file is part of Hibernate Spatial, an extension to the 
  * hibernate ORM solution for geographic data. 
- *  
+ *
  * Copyright © 2007 Geovise BVBA
  * Copyright © 2007 K.U. Leuven LRD, Spatial Applications Division, Belgium
  *
@@ -32,239 +32,242 @@ import com.vividsolutions.jts.geom.*;
 
 public class MultiMLineString extends MultiLineString implements MGeometry {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
 
-	private final double mGap; // difference in m between end of one part and
+    private final double mGap; // difference in m between end of one part and
 
-	private boolean monotone = false;
+    private boolean monotone = false;
 
-	private boolean strictMonotone = false;
+    private boolean strictMonotone = false;
 
-	/**
-	 * @param MlineStrings
-	 *            the <code>MLineString</code>s for this
-	 *            <code>MultiMLineString</code>, or <code>null</code> or an
-	 *            empty array to create the empty geometry. Elements may be
-	 *            empty <code>LineString</code>s, but not <code>null</code>s.
-	 */
-	public MultiMLineString(MLineString[] MlineStrings, double mGap,
-			GeometryFactory factory) {
-		super(MlineStrings, factory);
-		this.mGap = mGap;
-		determineMonotone();
-	}
+    /**
+     * @param MlineStrings the <code>MLineString</code>s for this
+     *                     <code>MultiMLineString</code>, or <code>null</code> or an
+     *                     empty array to create the empty geometry. Elements may be
+     *                     empty <code>LineString</code>s, but not <code>null</code>s.
+     */
+    public MultiMLineString(MLineString[] MlineStrings, double mGap,
+                            GeometryFactory factory) {
+        super(MlineStrings, factory);
+        this.mGap = mGap;
+        determineMonotone();
+    }
 
-	/**
-	 * TODO Improve this, and add more unit tests
-	 */
-	private void determineMonotone() {
-		this.monotone = true;
-		this.strictMonotone = true;
-		if (this.isEmpty()) {
-			return;
-		}
-		int mdir = ((MLineString) this.geometries[0]).getMeasureDirection();
-		for (int i = 0; i < this.geometries.length; i++) {
-			MLineString ml = (MLineString) this.geometries[i];
-			// check whether mlinestrings are all pointing in same direction,
-			// and
-			// are monotone
-			if (!ml.isMonotone(false)
-					|| (ml.getMeasureDirection() != mdir && !(ml
-							.getMeasureDirection() == MGeometry.CONSTANT))) {
-				this.monotone = false;
-				break;
-			}
+    /**
+     * TODO Improve this, and add more unit tests
+     */
+    private void determineMonotone() {
+        this.monotone = true;
+        this.strictMonotone = true;
+        if (this.isEmpty()) {
+            return;
+        }
+        int mdir = ((MLineString) this.geometries[0]).getMeasureDirection();
+        for (int i = 0; i < this.geometries.length; i++) {
+            MLineString ml = (MLineString) this.geometries[i];
+            // check whether mlinestrings are all pointing in same direction,
+            // and
+            // are monotone
+            if (!ml.isMonotone(false)
+                    || (ml.getMeasureDirection() != mdir && !(ml
+                    .getMeasureDirection() == MGeometry.CONSTANT))) {
+                this.monotone = false;
+                break;
+            }
 
-			if (!ml.isMonotone(true) || (ml.getMeasureDirection() != mdir)) {
-				this.strictMonotone = false;
-				break;
-			}
+            if (!ml.isMonotone(true) || (ml.getMeasureDirection() != mdir)) {
+                this.strictMonotone = false;
+                break;
+            }
 
-			// check whether the geometry measures do not overlap or
-			// are inconsistent with previous parts
-			if (i > 0) {
-				MLineString mlp = (MLineString) this.geometries[i - 1];
-				if (mdir == MGeometry.INCREASING) {
-					if (mlp.getMaxM() > ml.getMinM()) {
-						monotone = false;
-					} else if (mlp.getMaxM() >= ml.getMinM()) {
-						strictMonotone = false;
-					}
-				} else {
-					if (mlp.getMinM() < ml.getMaxM()) {
-						monotone = false;
-					} else if (mlp.getMinM() <= ml.getMaxM()) {
-						strictMonotone = false;
-					}
-				}
-
-			}
-
-		}
-		if (!monotone) {
-			this.strictMonotone = false;
-		}
-
-	}
-
-	protected void geometryChangedAction() {
-		determineMonotone();
-	}
-
-	public String getGeometryType() {
-		return "MultiMLineString";
-	}
-
-	public double getMGap() {
-		return this.mGap;
-	}
-
-	public double getMatCoordinate(Coordinate co, double tolerance)
-			throws MGeometryException {
-
-		if (!this.isMonotone(false)) {
-			throw new MGeometryException(
-					MGeometryException.OPERATION_REQUIRES_MONOTONE);
-		}
-
-		double mval = Double.NaN;
-		double dist = Double.POSITIVE_INFINITY;
-
-		com.vividsolutions.jts.geom.Point p = this.getFactory().createPoint(co);
-
-		// find points within tolerance for getMatCoordinate
-		for (int i = 0; i < this.getNumGeometries(); i++) {
-			MLineString ml = (MLineString) this.getGeometryN(i);
-			// go to next MLineString if the input point is beyond tolerance
-			if (ml.distance(p) > tolerance)
-				continue;
-
-			MCoordinate mc = ml.getClosestPoint(co, tolerance);
-			if (mc != null) {
-				double d = mc.distance(co);
-				if (d <= tolerance && d < dist) {
-					dist = d;
-					mval = mc.m;
-				}
-			}
-		}
-		return mval;
-	}
-
-	public Object clone() {
-		MultiLineString ml = (MultiLineString) super.clone();
-		return ml;
-	}
-
-	public void measureOnLength(boolean keepBeginMeasure) {
-		double startM = 0.0;
-		for (int i = 0; i < this.getNumGeometries(); i++) {
-			MLineString ml = (MLineString) this.getGeometryN(i);
-			if (i == 0) {
-				ml.measureOnLength(keepBeginMeasure);
-			} else {
-				ml.measureOnLength(false);
-			}
-			if (startM != 0.0) {
-				ml.shiftMeasure(startM);
-			}
-			startM += ml.getLength() + mGap;
-		}
-		this.geometryChanged();
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.hibernatespatial.mgeom.MGeometry#getCoordinateAtM(double)
-	 */
-	public Coordinate getCoordinateAtM(double m) throws MGeometryException {
-
-		if (!this.isMonotone(false)) {
-			throw new MGeometryException(
-					MGeometryException.OPERATION_REQUIRES_MONOTONE);
-		}
-
-		Coordinate c = null;
-		for (int i = 0; i < this.getNumGeometries(); i++) {
-			MGeometry mg = (MGeometry) this.getGeometryN(i);
-			c = mg.getCoordinateAtM(m);
-			if (c != null) {
-				return c;
-			}
-		}
-		return null;
-	}
-
-	public CoordinateSequence[] getCoordinatesBetween(double begin, double end)
-			throws MGeometryException {
-
-		if (!this.isMonotone(false)) {
-			throw new MGeometryException(
-					MGeometryException.OPERATION_REQUIRES_MONOTONE,
-                    "Operation requires geometry with monotonic measures");
-		}
-
-		if (this.isEmpty())
-			return null;
-
-		java.util.ArrayList<CoordinateSequence> ar = new java.util.ArrayList<CoordinateSequence>();
-
-		for (int i = 0; i < this.getNumGeometries(); i++) {
-			MLineString ml = (MLineString) this.getGeometryN(i);
-			for (CoordinateSequence cs : ml.getCoordinatesBetween(begin, end)) {
-                if (cs.size() > 0) {
-				    ar.add(cs);
+            // check whether the geometry measures do not overlap or
+            // are inconsistent with previous parts
+            if (i > 0) {
+                MLineString mlp = (MLineString) this.geometries[i - 1];
+                if (mdir == MGeometry.INCREASING) {
+                    if (mlp.getMaxM() > ml.getMinM()) {
+                        monotone = false;
+                    } else if (mlp.getMaxM() >= ml.getMinM()) {
+                        strictMonotone = false;
+                    }
+                } else {
+                    if (mlp.getMinM() < ml.getMaxM()) {
+                        monotone = false;
+                    } else if (mlp.getMinM() <= ml.getMaxM()) {
+                        strictMonotone = false;
+                    }
                 }
-			}
-		}
-		return ar.toArray(new CoordinateSequence[ar.size()]);
-	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.hibernatespatial.mgeom.MGeometry#getMinM()
-	 */
-	public double getMinM() {
-		double minM = Double.POSITIVE_INFINITY;
-		for (int i = 0; i < this.getNumGeometries(); i++) {
-			MLineString ml = (MLineString) this.getGeometryN(i);
-			double d = ml.getMinM();
-			if (d < minM)
-				minM = d;
-		}
-		return minM;
-	}
+            }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.hibernatespatial.mgeom.MGeometry#getMaxM()
-	 */
-	public double getMaxM() {
-		double maxM = Double.NEGATIVE_INFINITY;
-		for (int i = 0; i < this.getNumGeometries(); i++) {
-			MLineString ml = (MLineString) this.getGeometryN(i);
-			double d = ml.getMaxM();
-			if (d < maxM)
-				maxM = d;
-		}
-		return maxM;
-	}
+        }
+        if (!monotone) {
+            this.strictMonotone = false;
+        }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.hibernatespatial.mgeom.MGeometry#isMonotone()
-	 */
-	public boolean isMonotone(boolean strictMonotone) {
-		return strictMonotone ? this.strictMonotone : monotone;
-	}
+    }
+
+    protected void geometryChangedAction() {
+        determineMonotone();
+    }
+
+    public String getGeometryType() {
+        return "MultiMLineString";
+    }
+
+    public double getMGap() {
+        return this.mGap;
+    }
+
+    public double getMatCoordinate(Coordinate co, double tolerance)
+            throws MGeometryException {
+
+        if (!this.isMonotone(false)) {
+            throw new MGeometryException(
+                    MGeometryException.OPERATION_REQUIRES_MONOTONE);
+        }
+
+        double mval = Double.NaN;
+        double dist = Double.POSITIVE_INFINITY;
+
+        com.vividsolutions.jts.geom.Point p = this.getFactory().createPoint(co);
+
+        // find points within tolerance for getMatCoordinate
+        for (int i = 0; i < this.getNumGeometries(); i++) {
+            MLineString ml = (MLineString) this.getGeometryN(i);
+            // go to next MLineString if the input point is beyond tolerance
+            if (ml.distance(p) > tolerance)
+                continue;
+
+            MCoordinate mc = ml.getClosestPoint(co, tolerance);
+            if (mc != null) {
+                double d = mc.distance(co);
+                if (d <= tolerance && d < dist) {
+                    dist = d;
+                    mval = mc.m;
+                }
+            }
+        }
+        return mval;
+    }
+
+    public Object clone() {
+        MultiLineString ml = (MultiLineString) super.clone();
+        return ml;
+    }
+
+    public void measureOnLength(boolean keepBeginMeasure) {
+        double startM = 0.0;
+        for (int i = 0; i < this.getNumGeometries(); i++) {
+            MLineString ml = (MLineString) this.getGeometryN(i);
+            if (i == 0) {
+                ml.measureOnLength(keepBeginMeasure);
+            } else {
+                ml.measureOnLength(false);
+            }
+            if (startM != 0.0) {
+                ml.shiftMeasure(startM);
+            }
+            startM += ml.getLength() + mGap;
+        }
+        this.geometryChanged();
+    }
+
+    /*
+      * (non-Javadoc)
+      *
+      * @see org.hibernatespatial.mgeom.MGeometry#getCoordinateAtM(double)
+      */
+
+    public Coordinate getCoordinateAtM(double m) throws MGeometryException {
+
+        if (!this.isMonotone(false)) {
+            throw new MGeometryException(
+                    MGeometryException.OPERATION_REQUIRES_MONOTONE);
+        }
+
+        Coordinate c = null;
+        for (int i = 0; i < this.getNumGeometries(); i++) {
+            MGeometry mg = (MGeometry) this.getGeometryN(i);
+            c = mg.getCoordinateAtM(m);
+            if (c != null) {
+                return c;
+            }
+        }
+        return null;
+    }
+
+    public CoordinateSequence[] getCoordinatesBetween(double begin, double end)
+            throws MGeometryException {
+
+        if (!this.isMonotone(false)) {
+            throw new MGeometryException(
+                    MGeometryException.OPERATION_REQUIRES_MONOTONE,
+                    "Operation requires geometry with monotonic measures");
+        }
+
+        if (this.isEmpty())
+            return null;
+
+        java.util.ArrayList<CoordinateSequence> ar = new java.util.ArrayList<CoordinateSequence>();
+
+        for (int i = 0; i < this.getNumGeometries(); i++) {
+            MLineString ml = (MLineString) this.getGeometryN(i);
+            for (CoordinateSequence cs : ml.getCoordinatesBetween(begin, end)) {
+                if (cs.size() > 0) {
+                    ar.add(cs);
+                }
+            }
+        }
+        return ar.toArray(new CoordinateSequence[ar.size()]);
+    }
+
+    /*
+      * (non-Javadoc)
+      *
+      * @see org.hibernatespatial.mgeom.MGeometry#getMinM()
+      */
+
+    public double getMinM() {
+        double minM = Double.POSITIVE_INFINITY;
+        for (int i = 0; i < this.getNumGeometries(); i++) {
+            MLineString ml = (MLineString) this.getGeometryN(i);
+            double d = ml.getMinM();
+            if (d < minM)
+                minM = d;
+        }
+        return minM;
+    }
+
+    /*
+      * (non-Javadoc)
+      *
+      * @see org.hibernatespatial.mgeom.MGeometry#getMaxM()
+      */
+
+    public double getMaxM() {
+        double maxM = Double.NEGATIVE_INFINITY;
+        for (int i = 0; i < this.getNumGeometries(); i++) {
+            MLineString ml = (MLineString) this.getGeometryN(i);
+            double d = ml.getMaxM();
+            if (d > maxM)
+                maxM = d;
+        }
+        return maxM;
+    }
+
+    /*
+      * (non-Javadoc)
+      *
+      * @see org.hibernatespatial.mgeom.MGeometry#isMonotone()
+      */
+
+    public boolean isMonotone(boolean strictMonotone) {
+        return strictMonotone ? this.strictMonotone : monotone;
+    }
 
     public Geometry asGeometry() {
         return this;
